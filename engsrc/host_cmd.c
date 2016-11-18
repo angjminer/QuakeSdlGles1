8991387e5f05d67dd0fb155fc588679bca296d57
angelo miner <angjminer@gulftel.com>
2016-11-18 11:56:26 -0600

    Add files via upload

--- engsrc/host_cmd.c
+++ engsrc/host_cmd.c
@@ -781,7 +781,8 @@ void Host_Say(qboolean teamonly)
 	client_t *save;
 	int		j;
 	char	*p;
-	char*__restrict text;//[64];//unsigned char	text[64];
+	//char*__restrict text;//[64];//
+	unsigned char	text[64];
 	qboolean	fromServer = false;
 
 	if (cmd_source == src_command)
